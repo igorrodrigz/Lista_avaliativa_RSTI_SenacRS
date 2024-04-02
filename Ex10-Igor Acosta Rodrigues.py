3246c904bbceee3b9070347cba5ef710cc75fe16
Igor Rodrigues <137235250+igorrodrigz@users.noreply.github.com>
2024-04-02 10:48:53 -0300

    Update Ex10-Igor Acosta Rodrigues.py

--- Ex10-Igor Acosta Rodrigues.py
+++ Ex10-Igor Acosta Rodrigues.py
@@ -1,4 +1,4 @@
-print("Bem vindo ao Calculador de Terrenos 4.0 da Energon")
+print("Bem vindo ao Calculador de Terrenos disponíveis para a instalação\n de placas solares da Energon")
 print(20 * "~~")
 
 def calcularArea(comprimento, largura, *areas_menores):
@@ -13,35 +13,35 @@ def excluir(areas_menores):
         if 0 <= indice < len(areas_menores):
             del areas_menores[indice]
             print("Terrenos menores removidos com sucesso.")
-            return calcularArea()
+            return
         else:
             print("Índice inválido. Nenhum terreno foi removido.")
     else:
         print("Não há mais terrenos para excluir.")
 
-def main():
-    comprimento_terreno = float(input("Digite o comprimento do terreno em metros: "))
-    largura_terreno = float(input("Digite a largura do terreno em metros: "))
 
-    areas_menores = []
-    while input("""
-    Deseja adicionar uma área menor?
-    [s] para Sim
-    [n] para Não
-    """) == "s":
-        areas_menores.append(float(input("Digite a área menor a ser adicionada em metros quadrados: ")))
-    global areaFinal
-    areaFinal = calcularArea(comprimento_terreno, largura_terreno, *areas_menores)
-    print(f"A área total para instalação das placas solares é de {areaFinal} metros quadrados.")
+comprimento_terreno = float(input("Digite o comprimento do terreno em metros: "))
+largura_terreno = float(input("Digite a largura do terreno em metros: "))
 
-    if areas_menores and input("""
-    Deseja excluir alguma área menor?
-    [s] para Sim
-    [n] para Não
-    """) == "s":
-        excluir(areas_menores)
-        #print(areaFinal)
+areas_menores = []
+while input("""
+Deseja adicionar uma área menor?
+[s] para Sim
+[n] para Não
+""") == "s":
+    areas_menores.append(float(input("Digite a área menor a ser adicionada em metros quadrados: ")))
 
+areaFinal = calcularArea(comprimento_terreno, largura_terreno, *areas_menores)
+print(f"A área total para instalação das placas solares é de {areaFinal} metros quadrados.")
 
-if __name__ == '__main__':
-    main()
+if areas_menores and input("""
+Deseja excluir alguma área menor?
+[s] para Sim
+[n] para Não
+""") == "s":
+    excluir(areas_menores)
+    areaFinal2 = calcularArea(comprimento_terreno, largura_terreno, *areas_menores)
+    print(f"O Resultado final de areas para instalação das placas solares é {areaFinal2} metros")
+else:
+    areaFinal3 = calcularArea(comprimento_terreno, largura_terreno, *areas_menores)
+    print(f"Muito bem, o resultado final de areas disponíveis para a instalação das placas solares é {areaFinal3}")
